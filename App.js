8ebf0e269e41c2d98204c52f17bee2e18effebc0
UMREDKAR <106841969+UMREDKAR@users.noreply.github.com>
2023-08-13 04:23:58 +0530

    App.js

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "./Components/Header";
+import Body from "./Components/Body";
+
+
+function App() {
+  return (
+    // <h1 className='text-5xl text-red-500 bg-slate-400'>hello There !</h1>
+    <div>
+      <Header></Header>
+      <Body></Body>
+      
+    </div>
+  );
+}
+
+export default App;
