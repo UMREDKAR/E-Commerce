e0adb3fc7652b5479494ff5778f572d2bd278f3e
UMREDKAR <106841969+UMREDKAR@users.noreply.github.com>
2023-08-19 01:23:35 +0530

    App3.js

--- App.js
+++ App.js
@@ -1,17 +1,100 @@
-import React from "react";
+import React, { createContext, useState } from "react";
 import Header from "./Components/Header";
-import Body from "./Components/Body";
+import Cart from "./Components/Cart";
+import RealBody from "./Components/RealBody";
+import Data from "./Data";
+import See from "./Components/See";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import About from "./Components/About";
 
 
+
+// const Ctx = createContext();
 function App() {
+
+//  const[get,setGet] = useState(0);
+ const [cartItems,setCartItems] = useState([]);
+ 
+
+ const {products} = Data;
+  // console.log(products);
+  
+
+ const onAdd = (pro)=>{
+   const exist = cartItems.find((x)=> x.id===pro.id);
+   if(exist)
+   {
+     const newItems = cartItems.map((x)=>
+        x.id===pro.id ? {...exist, qty: exist.qty+1} : x
+     );
+     setCartItems(newItems);
+   }
+   else
+   {
+    const newItems = [...cartItems,{...pro, qty: 1}];
+    setCartItems(newItems);
+   }
+ }
+
+ const onRemove = (pro) =>{
+   
+  const exist = cartItems.find((x)=> x.id===pro.id);
+  if(exist.qty===1)
+  {
+    const newItems = cartItems.filter((x) => x.id !== pro.id);
+    setCartItems(newItems);
+  }
+  else
+  {
+     const newItems = cartItems.map((x)=>
+       x.id===pro.id ? {...exist, qty: exist.qty-1} : x
+     );
+     setCartItems(newItems);
+  }
+ }
+ 
+  
+
+ 
+  // function dataHandler(data)
+  // {
+  //    setGet(data);
+  // }
+ 
+  // console.log(cartItems);
+
   return (
-    // <h1 className='text-5xl text-red-500 bg-slate-400'>hello There !</h1>
-    <div>
-      <Header></Header>
-      <Body></Body>
+    <BrowserRouter>
+    
+      {/* <Ctx.Provider value={get}> */}
+      {/* <Header></Header>
+      <Cart 
+        onAdd={onAdd}
+        onRemove={onRemove}
+        cartItems={cartItems}/>
+      <RealBody 
+        product={products} 
+        onAdd={onAdd}
+        onRemove={onRemove}
+        cartItems={cartItems}/>
+        <See/> */}
+        <Routes>
+          <Route path="/"  element={
+          <>
+            <Header/>
+            <Cart onAdd={onAdd} onRemove={onRemove}
+                cartItems={cartItems}/> 
+            <RealBody product={products} onAdd={onAdd}
+                onRemove={onRemove} cartItems={cartItems}/>
+            <See/>
+          </> }/>
       
-    </div>
+          <Route path="/about"  element={<About/>}/>
+        </Routes>
+      {/* </Ctx.Provider>  */}
+    </BrowserRouter>
   );
 }
 
 export default App;
+// export { Ctx };
