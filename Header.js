2cf70cac991522c87a33fcf2a0313c61c6a1b93b
UMREDKAR <106841969+UMREDKAR@users.noreply.github.com>
2023-08-19 01:23:57 +0530

    Header2.js

--- Header.js
+++ Header.js
@@ -1,15 +1,29 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+
 const Header = () =>{
     return(
     <div>
         <div className='bg-black  text-white text-lg flex space-x-4 justify-center p-2'>
             <button className='px-10'>HOME</button>
-            <button className='px-10'>STORE</button>
-            <button className='px-10'>ABOUT</button>
+            <button className='px-10'>
+                <Link to="/">
+                   STORE 
+                </Link>
+               
+            </button>
+            <button className='px-10'>
+                <Link to="/about">
+                   ABOUT 
+                </Link>
+                
+            </button>
         </div>
 
-        <button className='absolute top-0 right-0 h-8 w-16 text-white text-base
+        {/* <button className='absolute top-0 right-0 h-8 w-16 text-white text-base
             border-solid border-2 border-cyan-300 rounded-lg mt-2 mr-9 mb-2'>
-            CART</button>
+            CART</button> */}
         <hr className='p-0.5 bg-red-800'/>    
         <div className='bg-neutral-500 text-white text-8xl flex justify-center pb-20'> 
             <h1>The Generics</h1>
