5490ed64717f4e9085a3fedcea9f06d46696333d
UMREDKAR <106841969+UMREDKAR@users.noreply.github.com>
2023-08-25 05:15:00 +0530

    App4.js

--- App.js
+++ App.js
@@ -4,8 +4,12 @@ import Cart from "./Components/Cart";
 import RealBody from "./Components/RealBody";
 import Data from "./Data";
 import See from "./Components/See";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes} from "react-router-dom";
 import About from "./Components/About";
+import Home from "./Components/Home";
+import Contact from "./Components/Contact";
+import ProductDetail from "./Components/ProductDetail";
+
 
 
 
@@ -52,7 +56,22 @@ function App() {
      setCartItems(newItems);
   }
  }
+
+ async function addUserHandler(userDetails)
+ {
+
+  const response = await fetch("https://react-api-cdb29-default-rtdb.firebaseio.com/userDetails.json",{
+                  method: 'POST',
+                  body: JSON.stringify(userDetails),
+                  headers:{
+                    'Content-Type': 'application/json'
+                  }
+  });
+
+  //  console.log(userDetails);
+ }
  
+
   
 
  
@@ -78,19 +97,27 @@ function App() {
         onRemove={onRemove}
         cartItems={cartItems}/>
         <See/> */}
+        
         <Routes>
           <Route path="/"  element={
           <>
             <Header/>
             <Cart onAdd={onAdd} onRemove={onRemove}
                 cartItems={cartItems}/> 
+
             <RealBody product={products} onAdd={onAdd}
                 onRemove={onRemove} cartItems={cartItems}/>
+                
             <See/>
           </> }/>
-      
+          <Route path="/:productID" element={<ProductDetail/>}/>
+            
+          
           <Route path="/about"  element={<About/>}/>
+          <Route path="/home"  element={<Home/>}/>
+          <Route path="/contact"  element={<Contact onAddUser={addUserHandler}/>}/>
         </Routes>
+       
       {/* </Ctx.Provider>  */}
     </BrowserRouter>
   );
